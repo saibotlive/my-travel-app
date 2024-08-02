c576c13e420c661ece5db7d31453417039beb404
Tobias Ighofose <saibotlive@gmail.com>
2024-08-02 14:34:13 +0100

    fix: Updatee caching

--- app/folder/[id]/page.tsx
+++ app/folder/[id]/page.tsx
@@ -11,7 +11,12 @@ async function getData(endpoint: string) {
   const baseUrl = process.env.BASE_URL || 'http://localhost:3000'; // Replace with your deployment URL or use environment variable
 
   try {
-    const res = await fetch(`${baseUrl}${endpoint}`, { cache: 'no-store' });
+    const res = await fetch(`${baseUrl}${endpoint}`, {
+      cache: 'no-store',
+      next: {
+        revalidate: 0,
+      },
+    });
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
